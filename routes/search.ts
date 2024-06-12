7d598038ea13b1ac8b588f30d0f7c7fe79c3daa0
Giorgos Siachamis <georgiossiach@gmail.com>
2024-06-12 11:50:17 +0300

    LLM fixes for SQLi and vulnerable JS library

--- routes/search.ts
+++ routes/search.ts
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2014-2023 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
-
 import models = require('../models/index')
 import { type Request, type Response, type NextFunction } from 'express'
 import { UserModel } from '../models/user'
@@ -15,12 +10,16 @@ class ErrorWithParent extends Error {
   parent: Error | undefined
 }
 
-// vuln-code-snippet start unionSqlInjectionChallenge dbSchemaChallenge
 module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
+
+    // Use parameterized queries to prevent SQL injection
+    const query = `SELECT * FROM Products WHERE ((name LIKE :searchTerm OR description LIKE :searchTerm) AND deletedAt IS NULL) ORDER BY name`;
+    const replacements = { searchTerm: `%${criteria}%` };
+
+    models.sequelize.query(query, { replacements })
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
         if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
@@ -60,7 +59,7 @@ module.exports = function searchProducts () {
               }
             }
           })
-        } // vuln-code-snippet hide-end
+        } 
         for (let i = 0; i < products.length; i++) {
           products[i].name = req.__(products[i].name)
           products[i].description = req.__(products[i].description)
@@ -71,4 +70,3 @@ module.exports = function searchProducts () {
       })
   }
 }
-// vuln-code-snippet end unionSqlInjectionChallenge dbSchemaChallenge
